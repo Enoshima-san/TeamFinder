df039083280b435f389157030ff580b6951b54e7
Ilyas Fardaoui <ilyasfardaoui@gmail.com>
2026-03-13 15:08:47 +0000

    AI: improve Python file src\teamup\application\repositories\__init__.py

--- src/teamup/application/repositories/__init__.py
+++ src/teamup/application/repositories/__init__.py
@@ -1,3 +1,7 @@
+```python
+# Import the IUserRepository interface from the user_repository module
 from .user_repository import IUserRepository
 
+# Define the list of symbols to be imported when using this module
 __all__ = ["IUserRepository"]
+```
