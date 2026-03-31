5a5dcb7d2153dbc65d5f7b7d49cc5bc81e948f50
Karimov Dmitry <dimakarimov55555@gmail.com>
2026-03-31 17:54:17 +0700

    feat: create new routes
    /user - user operations
    /<some announcement path>/responses - responses
    /games - games

--- src/teamup/api/routes/__init__.py
+++ src/teamup/api/routes/__init__.py
@@ -1,4 +1,13 @@
 from .auth import auth_router
 from .feed import feed_router
+from .games import games_router
+from .responses import responses_router
+from .users import user_router
 
-__all__ = ["auth_router", "feed_router"]
+__all__ = [
+    "auth_router",
+    "feed_router",
+    "games_router",
+    "responses_router",
+    "user_router",
+]
