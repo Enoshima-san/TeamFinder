c692645fcb2b5a29b95af513ad925d75468ffdbc
Karimov Dmitry <dimakarimov55555@gmail.com>
2026-04-17 22:57:03 +0700

    fix: add Depends for repositories di

--- src/teamup/core/di/repositories.py
+++ src/teamup/core/di/repositories.py
@@ -1,3 +1,6 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from teamup.domain import (
     IAnnouncementRepository,
     IConversationRepository,
@@ -18,35 +21,52 @@ from teamup.infra import (
     UserGamesRepository,
     UserRepository,
 )
+from teamup.infra.database import get_async_session
 
 
-async def get_user_repository(session) -> IUserRepository:
+async def get_user_repository(
+    session: AsyncSession = Depends(get_async_session),
+) -> IUserRepository:
     return UserRepository(session)
 
 
-async def get_game_repository(session) -> IGameRepository:
+async def get_game_repository(
+    session: AsyncSession = Depends(get_async_session),
+) -> IGameRepository:
     return GameRepository(session)
 
 
-async def get_user_games_repository(session) -> IUserGamesRepository:
+async def get_user_games_repository(
+    session: AsyncSession = Depends(get_async_session),
+) -> IUserGamesRepository:
     return UserGamesRepository(session)
 
 
-async def get_announcement_repository(session) -> IAnnouncementRepository:
+async def get_announcement_repository(
+    session: AsyncSession = Depends(get_async_session),
+) -> IAnnouncementRepository:
     return AnnouncementRepository(session)
 
 
-async def get_response_repository(session) -> IResponseRepository:
+async def get_response_repository(
+    session: AsyncSession = Depends(get_async_session),
+) -> IResponseRepository:
     return ResponseRepository(session)
 
 
-async def get_conversation_repository(session) -> IConversationRepository:
+async def get_conversation_repository(
+    session: AsyncSession = Depends(get_async_session),
+) -> IConversationRepository:
     return ConversationRepository(session)
 
 
-async def get_message_repository(session) -> IMessageRepository:
+async def get_message_repository(
+    session: AsyncSession = Depends(get_async_session),
+) -> IMessageRepository:
     return MessageRepository(session)
 
 
-async def get_notification_repository(session) -> INotificationRepository:
+async def get_notification_repository(
+    session: AsyncSession = Depends(get_async_session),
+) -> INotificationRepository:
     return NotificationRepository(session)
